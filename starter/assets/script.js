e8653591f4945b3c1e016f37551e99232efb8dcc
Sarahsegla <sarahsegla@icloud.com>
2023-01-22 00:47:28 +0000

    added vars querySelector

---
+++ starter/assets/script.js
@@ -0,0 +1,6 @@
+var timer = document.querySelector('#time');
+var highScore = document.querySelector('.score');
+var startButtom = document.querySelector('#start');
+var finalScore = document.querySelector('#final-score');
+var initials = document.querySelector('#initials');
+var submit = document.querySelector('#submit')
